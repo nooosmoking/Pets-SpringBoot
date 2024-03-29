cd5ac26ca5cc370cc8488c8b7c6c4e4ad309f0cc
Kaleigh Hyde <kaleighh@ob-j8.nsk.21-school.ru>
2024-03-29 21:09:30 +0700

    new kitten page

---
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/kitten")
+    public String kitten(){
+
+        return "kitten.html";
+    }
+}
